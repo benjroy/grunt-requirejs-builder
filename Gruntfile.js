66a59aaff272cef8536d96874810b01e8be463e9
Benjamin Allen <ben@bittorrent.com>
2014-03-20 21:40:37 -0700

    gruntwork

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/*
+* grunt-contrib-requirejs-builder
+* http://gruntjs.com/
+*
+* Author: Benjamin Allen
+*/
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // By default, lint.
+    grunt.registerTask('default', ['jshint']);
+};
